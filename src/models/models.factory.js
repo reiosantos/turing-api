3240ef70692869f00524f47586b003281a808854
Reiosantos <ronireiosantos@gmail.com>
2019-10-11 10:38:46 +0300

    Fetch Categories Fetch Departments

--- src/models/models.factory.js
+++ src/models/models.factory.js
@@ -1,6 +1,6 @@
 import models from '../database/models';
 
-const { Customer, Order, Product, ShoppingCart, ProductCategory, Category } = models;
+const { Customer, Order, Product, ShoppingCart, ProductCategory, Category, Department } = models;
 
 class ModelFactory {
 	/**
@@ -20,6 +20,7 @@ class ModelFactory {
 		if (modelName.match(/^shopping_cart?$/)) return ShoppingCart;
 		if (modelName.match(/^product_category$/)) return ProductCategory;
 		if (modelName.match(/^category$/)) return Category;
+		if (modelName.match(/^departments?$/)) return Department;
 
 		return null;
 	};
